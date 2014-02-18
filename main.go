44c7c7ab805922d26c40d0af8164375ea4f41358
Philip Horger <campadrenalin@gmail.com>
2014-02-17 18:18:49 -0800

    Split some responsibility into parse.go

--- main.go
+++ main.go
@@ -2,36 +2,11 @@ package main
 
 import (
 	"github.com/campadrenalin/go-deje"
-	"github.com/campadrenalin/go-deje/model"
 	"github.com/codegangsta/martini"
 	"github.com/codegangsta/martini-contrib/render"
 	"net/http"
 	"strconv"
 )
-
-func get_form(r *http.Request, key string) string {
-	values := r.Form[key]
-	if len(values) == 0 {
-		return ""
-	}
-	return values[0]
-}
-
-func get_location(r *http.Request) (*model.IRCLocation, error) {
-	r.ParseForm()
-	port, err := strconv.ParseUint(get_form(r, "port"), 10, 32)
-	if err != nil {
-		return nil, err
-	}
-
-	location := &model.IRCLocation{
-		Host:    get_form(r, "host"),
-		Port:    uint32(port),
-		Channel: get_form(r, "channel"),
-	}
-
-    return location, nil
-}
 
 func do_home(r render.Render) {
     r.HTML(200, "root", nil)
---
+++ parse.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/campadrenalin/go-deje/model"
+	"net/http"
+	"strconv"
+)
+
+func get_form(r *http.Request, key string) string {
+	values := r.Form[key]
+	if len(values) == 0 {
+		return ""
+	}
+	return values[0]
+}
+
+func get_location(r *http.Request) (*model.IRCLocation, error) {
+	r.ParseForm()
+	port, err := strconv.ParseUint(get_form(r, "port"), 10, 32)
+	if err != nil {
+		return nil, err
+	}
+
+	location := &model.IRCLocation{
+		Host:    get_form(r, "host"),
+		Port:    uint32(port),
+		Channel: get_form(r, "channel"),
+	}
+
+    return location, nil
+}
