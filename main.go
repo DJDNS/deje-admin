ef0be47d54038e2b5075d958f723905ee561cc87
Philip Horger <campadrenalin@gmail.com>
2014-02-17 18:13:55 -0800

    Start using templates

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"github.com/campadrenalin/go-deje"
 	"github.com/campadrenalin/go-deje/model"
 	"github.com/codegangsta/martini"
-	"log"
+	"github.com/codegangsta/martini-contrib/render"
 	"net/http"
 	"strconv"
 )
@@ -17,41 +17,33 @@ func get_form(r *http.Request, key string) string {
 	return values[0]
 }
 
-func do_home() string {
-	return `<!DOCTYPE html>
-<html>
-<head>
-    <title>DEJE Admin Interface</title>
-</head>
-<body>
-
-<h1>DEJE Admin Interface</h1>
-Open document by IRC location:
-<form action="/open" method="get">
-    <label>IRC server host<input name="host"/></label><br/>
-    <label>IRC server port<input name="port"/></label><br/>
-    <label>#channel<input name="channel"/></label><br/>
-    <input type="submit"/>
-</form>
-
-</body>
-</html>
-`
-}
-
-func do_open(r *http.Request, l *log.Logger, c *deje.DEJEController) (int, string) {
+func get_location(r *http.Request) (*model.IRCLocation, error) {
 	r.ParseForm()
 	port, err := strconv.ParseUint(get_form(r, "port"), 10, 32)
 	if err != nil {
-		return 500, err.Error()
+		return nil, err
 	}
-	location := model.IRCLocation{
+
+	location := &model.IRCLocation{
 		Host:    get_form(r, "host"),
 		Port:    uint32(port),
 		Channel: get_form(r, "channel"),
 	}
-	doc := c.GetDocument(location)
 
+    return location, nil
+}
+
+func do_home(r render.Render) {
+    r.HTML(200, "root", nil)
+}
+
+func do_open(r *http.Request, c *deje.DEJEController) (int, string) {
+    location, err := get_location(r)
+    if err != nil {
+        return 500, err.Error()
+    }
+
+	doc := c.GetDocument(*location)
 	length := strconv.FormatUint(uint64(doc.Events.Length()), 10)
 	return 200, length
 }
@@ -60,6 +52,7 @@ func main() {
 	controller := deje.NewDEJEController()
 	m := martini.Classic()
 	m.Map(controller)
+    m.Use(render.Renderer())
 
 	m.Get("/", do_home)
 	m.Get("/open", do_open)
