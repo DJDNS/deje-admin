cd9d02c150ba92b22627f7c4b91600519011cf0a
Philip Horger <campadrenalin@gmail.com>
2014-02-19 23:09:03 -0800

    Harder/better/faster/stronger way of doing subscription

--- socket.go
+++ socket.go
@@ -6,27 +6,29 @@ import (
 	"github.com/googollee/go-socket.io"
 	"log"
 	"net/http"
-	"time"
 )
 
-func sio_irc_loop(c *deje.DEJEController, ns *socketio.NameSpace) {
-	// Create stopper signal
-	stopper := make(chan interface{})
-	ns.Session.Values["stopper"] = stopper
+type strchan chan string
 
-	// Create channel listener (nil)
-	var channel chan string
-	ns.Session.Values["channel"] = channel
+func sio_irc_loop(c *deje.DEJEController, ns *socketio.NameSpace) {
+	defer log.Printf("Ending sio_irc_loop for %v", ns.Id())
+
+	// Set up stopper and cgetter chans
+	stopper := ns.Session.Values["stopper"].(chan interface{})
+	cgetter := ns.Session.Values["cgetter"].(chan strchan)
+
+	// Create IRC channel listener (nil)
+	var channel strchan
 
 	// Listen for any
 	for {
 		select {
 		case <-stopper:
-			break
+			return
 		case line := <-channel:
 			ns.Emit("output", line)
-		case <-time.After(1 * time.Second):
-			channel = ns.Session.Values["channel"].(chan string)
+		case channel = <-cgetter:
+			continue // Do nothing
 		}
 	}
 }
@@ -40,17 +42,24 @@ func run_sio(controller *deje.DEJEController) {
 
 	irc := sio.Of("/irc")
 	irc.On("connect", func(ns *socketio.NameSpace) {
+		ns.Session.Values["stopper"] = make(chan interface{})
+		ns.Session.Values["cgetter"] = make(chan strchan)
+		go sio_irc_loop(controller, ns)
+
 		id, endpoint := ns.Id(), ns.Endpoint()
 		log.Printf("connected: %v in channel %v", id, endpoint)
-		go sio_irc_loop(controller, ns)
 	})
 	irc.On("disconnect", func(ns *socketio.NameSpace) {
+		stopper := ns.Session.Values["stopper"].(chan interface{})
+		stopper <- nil
+
 		id, endpoint := ns.Id(), ns.Endpoint()
 		log.Printf("disconnected: %v in channel %v", id, endpoint)
 	})
 	irc.On("subscribe", func(ns *socketio.NameSpace, url string) {
 		channel := make(chan string)
-		ns.Session.Values["channel"] = channel
+		cgetter := ns.Session.Values["cgetter"].(chan strchan)
+		cgetter <- channel
 		channel <- "Subscribed to " + url
 	})
 
