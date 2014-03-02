13e454023d4c508ae62f71a6562d645d983e3d53
Philip Horger <campadrenalin@gmail.com>
2014-03-02 13:14:11 -0800

    Logging improvements
     * Smaller max-height, so that even with lots of lines,
       we still haven't totally screwed up page layout.
     * If scrolled to the bottom, stay scrolled to bottom when
       new lines come in.

--- static/js/djadmin/logger.js
+++ static/js/djadmin/logger.js
@@ -5,9 +5,21 @@ function Logger(selector) {
     this.element = $(selector);
 }
 
+Logger.prototype.scroll_position = function() {
+    return this.element.scrollTop();
+}
+Logger.prototype.scroll_max = function() {
+    var el = this.element;
+    return el.prop('scrollHeight') - el.innerHeight();
+}
+Logger.prototype.is_following = function() {
+    return (this.scroll_position() === this.scroll_max());
+}
+
 Logger.prototype.print = function(data) {
-    var old_content, new_content;
+    var old_content, new_content, following;
     old_content = this.element.text();
+    following = this.is_following();
 
     if (old_content.length == "") {
         new_content = data;
@@ -15,6 +27,10 @@ Logger.prototype.print = function(data) {
         new_content = old_content + '\n' + data;
     }
     this.element.text(new_content);
+
+    if (following) {
+        this.element.scrollTop(this.scroll_max());
+    }
 }
 
 return Logger;
