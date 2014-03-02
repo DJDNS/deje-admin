cf67b44821477025badf3d96f497f24e964095b0
Philip Horger <campadrenalin@gmail.com>
2014-03-01 17:02:15 -0800

    Split out logger JS

--- static/js/deje-admin.js
+++ static/js/deje-admin.js
@@ -1,5 +1,5 @@
 // Admin interface code
-define(['jquery', 'socket.io'], function($, io) {
+define(['djadmin/logger', 'socket.io'], function(logger, io) {
 
 function get_sio(resource) {
     return [
@@ -20,37 +20,24 @@ function IRCLogger(url) {
     this.socket.on("error",      this.on_error.bind(this));
 }
 
-IRCLogger.prototype.print = function(data) {
-    var logger, old_content, new_content;
-    logger = $('#irc_logger');
-    old_content = logger.text();
-
-    if (old_content.length == "") {
-        new_content = data;
-    } else {
-        new_content = old_content + '\n' + data;
-    }
-    logger.text(new_content);
-}
-
 IRCLogger.prototype.on_connect = function() {
-    this.print("Connected to DEJE Admin server");
+    logger.print("Connected to DEJE Admin server");
     this.subscribe(this.url);
 }
 
 IRCLogger.prototype.on_disconnect = function() {
-    this.print("Disconnected from DEJE Admin server");
+    logger.print("Disconnected from DEJE Admin server");
 }
 
 IRCLogger.prototype.on_output = function(line) {
-    this.print(line);
+    logger.print(line);
 }
 IRCLogger.prototype.on_error = function(line) {
-    this.print("ERROR: " + line);
+    logger.print("ERROR: " + line);
 }
 IRCLogger.prototype.subscribe = function(url) {
     this.url = url;
-    this.print("Subscribing to: " + url);
+    logger.print("Subscribing to: " + url);
     this.socket.emit("subscribe", url);
 }
 
